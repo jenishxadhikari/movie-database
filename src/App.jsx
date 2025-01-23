35a813ae66121abdc1b79b0dc9747801d98fdf93
jenishxadhikari <114239379+jenishxadhikari@users.noreply.github.com>
2025-01-23 23:07:59 +0545

    chore: setup initial project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button'
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <main className="flex flex-col items-center justify-center gap-4 py-5">
+        <h1 className="text-2xl font-bold">Movie Database</h1>
+        <Button>Hello World</Button>
+      </main>
+    </div>
+  )
+}
+
+export default App
